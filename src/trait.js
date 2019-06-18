d52031ca8268546ae06c3a129a69fc19d2e4bc94
Karolin Varner <karo@cupdev.net>
2019-06-18 13:38:11 +0200

    Version without pulling all of lodash

--- src/trait.js
+++ src/trait.js
@@ -10,7 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-const { isPlainObject } = require('lodash');
+const isPlainObject = require('lodash.isplainobject');
 const { curry } = require('./functional');
 const {
   isPrimitive, type, typename, isdef,
